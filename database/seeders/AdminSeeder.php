2c6b857f26e599db15d982a77e9ead43e2503839
Bhazyga <bhazyacyuta@gmail.com>
2025-10-24 07:01:20 +0000

    Penyesuaian Server

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -21,37 +21,11 @@ class AdminSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@sinau.com',
             'password' => 'password',
-            'role' => 'admin',
-        ]);
+            'role' => 'admin'
+	]);
 
-        $subscriber = Subscriber::create([
-            'nama_lengkap' => 'Bhazy Ghazalah Acyuta',
-            'tempat_lahir' => 'Jakarta',
-            'tanggal_lahir' => '1998-07-27',
-            'jenis_kelamin' => 'Laki-laki',
-            'alamat_subscriber' => 'Jl. Kenangan No. 1',
-            'provinsi_subscriber' => 'Jawa Timur',
-            'kota_kabupaten_subscriber' => 'Surabaya',
-            'nama_ayah' => 'Pak Bhazy',
-            'telepon_ayah' => '081234567890',
-            'nama_ibu' => 'Bu Tatum',
-            'telepon_ibu' => '081298765432',
-            'pekerjaan_ayah' => 'Karyawan',
-            'pekerjaan_ibu' => 'Ibu Rumah Tangga',
-            'alamat_ortu' => 'Jl. Kenangan No. 1',
-            'nama_sekolah_asal' => 'SMPN 2 Surabaya',
-            'jenjang_pendidikan_terakhir' => 'SMP',
-            'alamat_sekolah_asal' => 'Jl. Pendidikan No. 20',
-        ]);
-
-        User::create([
-            'name' => $subscriber->nama_lengkap,
-            'email' => 'sinau@pengguna.com',
-            'password' => Hash::make('password'),
-            'role' => 'subscriber',
-            'subscriber_id' => $subscriber->id,
-        ]);
     }
 
 
 }
+
