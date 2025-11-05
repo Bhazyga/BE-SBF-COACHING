aab19e4bc5d3abc3594f0a1e8bc11abf7ed45910
Bhazyga <bhazyacyuta@gmail.com>
2025-11-06 04:55:39 +0700

    Seeding Items Data

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -12,32 +12,35 @@ class ItemSeeder extends Seeder
     {
         $items = [
             [
+                'id' => 1,
                 'kode_item' => 'SBF-M1',
                 'nama' => 'Langganan 1 Bulan',
                 'deskripsi' => 'Akses penuh ke semua artikel dan fitur premium SBF Coaching selama 1 bulan.',
-                'harga' => 49000,
+                'harga' => 78200,
                 'tipe' => 'biaya',
                 'gambar' => 'langganan-1bulan.png',
                 'aktif' => true,
             ],
-            // [
-            //     'kode_item' => 'SBF-M6',
-            //     'nama' => 'Langganan 6 Bulan',
-            //     'deskripsi' => 'Akses penuh ke semua artikel dan fitur premium SBF Coaching selama 6 bulan.',
-            //     'harga' => 249000, // Rp249.000
-            //     'tipe' => 'biaya',
-            //     'gambar' => 'langganan-6bulan.png',
-            //     'aktif' => true,
-            // ],
-            // [
-            //     'kode_item' => 'SBF-Y1',
-            //     'nama' => 'Langganan 1 Tahun',
-            //     'deskripsi' => 'Akses penuh ke semua artikel dan fitur premium SBF Coaching selama 1 tahun.',
-            //     'harga' => 479000, // Rp479.000
-            //     'tipe' => 'biaya',
-            //     'gambar' => 'langganan-1tahun.png',
-            //     'aktif' => true,
-            // ],
+            [
+                'id' => 2,
+                'kode_item' => 'SBF-M6',
+                'nama' => 'Langganan 6 Bulan',
+                'deskripsi' => 'Akses penuh ke semua artikel dan fitur premium SBF Coaching selama 6 bulan.',
+                'harga' => 418900, // Rp249.000
+                'tipe' => 'biaya',
+                'gambar' => 'langganan-6bulan.png',
+                'aktif' => true,
+            ],
+            [
+                'id' => 3,
+                'kode_item' => 'SBF-Y1',
+                'nama' => 'Langganan 1 Tahun',
+                'deskripsi' => 'Akses penuh ke semua artikel dan fitur premium SBF Coaching selama 1 tahun.',
+                'harga' => 726050, // Rp479.000
+                'tipe' => 'biaya',
+                'gambar' => 'langganan-1tahun.png',
+                'aktif' => true,
+            ],
         ];
 
         foreach ($items as $item) {
